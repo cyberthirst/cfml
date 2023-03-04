b269c0571d5f467a129f9e1bcc5a1d4675ba7bac
Mirek Skrabal <mirekskrabal94@gmail.com>
2023-03-04 18:45:35 +0100

    add astdefinition

--- ast_interpreter.c
+++ ast_interpreter.c
@@ -5,10 +5,41 @@
 
 IState* init_interpreter() {
     IState *state = malloc(sizeof(IState));
-    state->mem = malloc(1024);
+    state->heap = malloc(sizeof(Heap));
+    state->heap->heap_start = malloc(MEM_SZ);
+    state->heap->heap_free = state->heap->heap_start;
+    state->heap->heap_size = 0;
     state->stack = malloc(sizeof(Environment) * MAX_STACK_SZ);
-    state->stack_size = 0;
+    state->current_env = -1;
+    state->globals = malloc(sizeof(Environment));
+    state->globals->var_cnt = 0;
     return state;
+}
+
+void add_to_env(Value val, Str name, IState *state) {
+    if (state->current_env == -1) { //global scope
+        if (state->globals->var_cnt == MAX_VARS) {
+            printf("Too many variables in global scope\n");
+            exit(1);
+        }
+        else {
+            size_t var_cnt = state->globals->var_cnt;
+            state->globals->vars[var_cnt].name = name;
+            state->globals->var_cnt++;
+        }
+    
+    } else { //local scope
+        if (state->stack[state->current_env].var_cnt == MAX_VARS) {
+            printf("Too many variables in local scope\n");
+            exit(1);
+        }
+        else {
+            size_t var_cnt = state->stack[state->current_env].var_cnt;
+            state->stack[state->current_env].vars[var_cnt].name = name;
+            state->stack[state->current_env].var_cnt++;
+        }
+
+    }    
 }
 
 Value interpret(Ast *ast, IState *state) {
@@ -24,14 +55,26 @@ Value interpret(Ast *ast, IState *state) {
         case AST_NULL: {
             return (Value) { .kind = VK_NULL };
         }
-        //case AST_DEFINITION: {return (Value){}; }
+        case AST_DEFINITION: {
+            AstDefinition *definition = (AstDefinition *) ast; 
+            Value val = interpret(definition->value, state);
+            add_to_env(val, definition->name, state);
+            return val;
+        }
         //case AST_VARIABLE_ACCESS: {return (Value){};}
         //case AST_VARIABLE_ASSIGNMENT: {return (Value){};}
         //case AST_FUNCTION: {return (Value){};}
         //case AST_FUNCTION_CALL: {return (Value){};}
         //case AST_PRINT: {return (Value){};}
         //case AST_BLOCK: {return (Value){};}
-        //case AST_TOP: {return (Value){};}
+        case AST_TOP: {
+            AstTop *top = (AstTop *) ast;
+            Value value;
+            for (size_t i = 0; i < top->expression_cnt; i++) {
+                value = interpret(top->expressions[i], state);
+            }
+            return value;
+        }
         //case AST_LOOP: {return (Value){};}
         //case AST_CONDITIONAL: {return (Value){};}
         default: {
--- ast_interpreter.h
+++ ast_interpreter.h
@@ -7,14 +7,6 @@
 #define MAX_VAR_NAME_LEN 64
 #define MEM_SZ 1024 * 1024 * 4
 
-typedef struct { Str vars[MAX_VARS]; int ret_val; } Environment;
-
-typedef struct {
-    void *mem;
-    Environment *stack;
-    size_t stack_size;
-} IState;
-
 typedef enum {
 	VK_NULL,
 	VK_BOOLEAN,
@@ -22,6 +14,41 @@ typedef enum {
 	VK_GCVALUE,
 	VK_FUNCTION,
 } ValueKind;
+
+typedef struct {
+    Str name;
+    ValueKind *values;
+} Variable;
+
+
+typedef struct {
+     Variable vars[MAX_VARS];
+     size_t var_cnt;
+     int ret_val;
+} Environment;
+
+typedef struct {
+    uint8_t *heap_start;
+    uint8_t *heap_free;
+    size_t heap_size;
+} Heap;
+
+typedef struct {
+    //ptr to the heap where we store the vars
+    Heap *heap;
+    //size of the currently allocated memory
+    size_t heap_size;
+    //stack of environments for functions
+    Environment *stack;
+    //ptr to the top of the stack
+    //if we have global scope, this is -1
+    int current_env;
+    //gloabl variables
+    Environment *globals;
+    //functions
+    //AstFunction *functions;
+    AstFunction **functions;
+} IState;
 
 typedef struct {
 	ValueKind kind;
@@ -32,5 +59,10 @@ typedef struct {
 	};
 } Value;
 
+//initializes the state of the interpreter
 IState* init_interpreter();
+
+void add_to_env(Value val, Str name, IState *state);
+
+//interprets the ast `ast` using the state `state
 Value interpret(Ast *ast, IState *state);
